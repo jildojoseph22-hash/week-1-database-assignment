bc6b7e32ec09b8bc58fa24f079599e0071ba45e8
jildojoseph22-hash <jildojoseph22@gmail.com>
2026-09-09 11:49:13 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,18 @@
-
+CREATE DATABASE library_management;
+SHOW DATABASES;
+USE library_management;
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    category VARCHAR(50),
+    publication_year INT
+);
+INSERT INTO books (title, author, category, publication_year)
+VALUES
+('Things Fall Apart', 'Chinua Achebe', 'Fiction', 1958),
+('The River Between', 'Ngugi wa Thiong''o', 'Fiction', 1965),
+('Clean Code', 'Robert C. Martin', 'Programming', 2008),
+('Introduction to Algorithms', 'Thomas H. Cormen', 'Computer Science', 2009),
+('Atomic Habits', 'James Clear', 'Self Development', 2018);
+SELECT * FROM books;
